0fca229692d5167ce678d819d79c60da29fbb7ab
Jacob Muchow <jacobamuchow@gmail.com>
2024-06-14 23:17:09 -0500

    Days _01 setup

--- 2016/advent/src/days/mod.rs
+++ 2016/advent/src/days/mod.rs
@@ -1,16 +1,1 @@
-use std::fs::read_to_string;
-
-fn read_lines(path: &str) -> Vec<String> {
-    read_to_string(path)
-        .unwrap()
-        .lines()
-        .map(String::from)
-        .collect()
-}
-
-pub fn run(path: &str) {
-    let lines = read_lines(path);
-    for line in lines {
-        println!("{}", line);
-    }
-}
+pub mod _01;
--- 2016/advent/src/main.rs
+++ 2016/advent/src/main.rs
@@ -1,5 +1,5 @@
 mod days;
 
 fn main() {
-    days::run("src/test.txt");
+    days::_01::run("src/test.txt");
 }
