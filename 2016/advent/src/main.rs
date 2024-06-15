91c750f1b33517c587ce83a39c83f11f28ca4e7a
Jacob Muchow <jacobamuchow@gmail.com>
2024-06-14 21:25:21 -0500

    Func to read lines to vec

--- 2016/advent/src/main.rs
+++ 2016/advent/src/main.rs
@@ -1,23 +1,16 @@
-use std::fs::File;
-use std::io::Read;
-use std::path::Path;
+use std::fs::read_to_string;
+
+fn read_lines(path: &str) -> Vec<String> {
+    read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect()
+}
 
 fn main() {
-    println!("Hello there");
-
-    let path = Path::new("src/input.txt");
-    println!("Going to read from file... '{}'", path.display());
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("Error opening file '{}': {}", path.display(), why),
-        Ok(file) => file,
-    };
-
-    let mut contents = String::new();
-    match file.read_to_string(&mut contents) {
-        Err(why) => panic!("Error reading data from file '{}', {}", path.display(), why),
-        Ok(_) => ()
+    let lines = read_lines("src/input.txt");
+    for line in lines {
+        println!("{}", line);
     }
-
-    println!("{}", contents);
 }
