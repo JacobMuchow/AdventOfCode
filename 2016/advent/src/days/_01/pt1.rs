a919b1f97133bdc161142e319d7239b0ba5781ef
Jacob Muchow <jacobamuchow@gmail.com>
2024-06-15 00:15:15 -0500

    Regex parsing instructions

--- 2016/advent/src/days/_01/pt1.rs
+++ 2016/advent/src/days/_01/pt1.rs
@@ -1,4 +1,5 @@
 use std::fs::read_to_string;
+use regex::Regex;
 
 fn read_lines(path: &str) -> Vec<String> {
     read_to_string(path)
@@ -10,7 +11,20 @@ fn read_lines(path: &str) -> Vec<String> {
 
 pub fn run() {
     let lines = read_lines("src/days/_01/input.txt");
-    for line in lines {
-        println!("{}", line);
+    let line = lines.get(0).unwrap();
+
+    println!("{}", line);
+
+    let re = Regex::new(r"([LR])([0-9]+)").unwrap();
+
+    let directions = re.captures_iter(line.as_str()).map(|caps| {
+        let dir = caps.get(1).map(|m| m.as_str()).unwrap();
+        let dist = caps.get(2).map(|m| m.as_str()).unwrap();
+
+        (dir, dist)
+    });
+
+    for dir in directions {
+        println!("{}{}", dir.0, dir.1);
     }
 }
