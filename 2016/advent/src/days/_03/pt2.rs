3f5955d62d18ce079666d51186db5d1c283fc929
Jacob Muchow <jacobamuchow@gmail.com>
2024-06-15 17:03:07 -0500

    pt 2 done

--- 2016/advent/src/days/_03/pt2.rs
+++ 2016/advent/src/days/_03/pt2.rs
@@ -1,44 +1,39 @@
-#![allow(dead_code)]
-
 use crate::shared::io::read_lines_from_file;
+use regex::Regex;
 
 pub fn run() {
-    let lines = read_lines_from_file("src/days/_02/input.txt");
+    let lines = read_lines_from_file("src/days/_03/input.txt");
 
-    let keypad = [
-        [".", ".", "1", ".", "."],
-        [".", "2", "3", "4", "."],
-        ["5", "6", "7", "8", "9"],
-        [".", "A", "B", "C", "."],
-        [".", ".", "D", ".", "."],
-    ];
-
-    let mut x = 0_i32;
-    let mut y = 2_i32;
-    let mut keycode = String::from("");
+    let mut numbers: Vec<(u32, u32, u32)> = Vec::new();
+    let re = Regex::new(r"([0-9]+)\s+([0-9]+)\s+([0-9]+)").unwrap();
 
     for line in lines {
-        for char in line.chars() {
-            let mut new_x = x;
-            let mut new_y = y;
+        let caps = re.captures(&line).unwrap();
 
-            match char {
-                'R' => new_x = (x+1).min(4),
-                'L' => new_x = (x-1).max(0),
-                'U' => new_y = (y-1).max(0),
-                'D' => new_y = (y+1).min(4),
-                c @ _ => panic!("Unknown char in input: '{}'", c)
-            };
-
-            if keypad[new_y as usize][new_x as usize] != "." {
-                x = new_x;
-                y = new_y;
-            }
-        }
-
-        keycode.push_str(&keypad[y as usize][x as usize]);
+        numbers.push((
+            caps[1].parse().unwrap(),
+            caps[2].parse().unwrap(),
+            caps[3].parse().unwrap(),
+        ))
     }
 
-    println!("Keycode: {}", keycode);
+    let mut potential_triangles: Vec<(u32, u32, u32)> = Vec::new();
+    for i in (0..numbers.len()).step_by(3) {
+        potential_triangles.push((numbers[i].0, numbers[i+1].0, numbers[i+2].0));
+        potential_triangles.push((numbers[i].1, numbers[i+1].1, numbers[i+2].1));
+        potential_triangles.push((numbers[i].2, numbers[i+1].2, numbers[i+2].2));
+    }
 
+    let mut num_possible = 0_u32;
+
+    for sides in potential_triangles {
+        let (s1, s2, s3) = sides;
+        let max = s1.max(s2).max(s3);
+
+        if s1+s2 > max && s1+s3 > max && s2+s3 > max {
+            num_possible += 1;
+        }
+    }
+
+    println!("Num possible: {}", num_possible);
 }
