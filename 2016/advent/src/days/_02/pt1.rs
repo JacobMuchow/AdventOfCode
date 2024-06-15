86becc2a88e4dd9812eceaa5a2dee4fd83568910
Jacob Muchow <jacobamuchow@gmail.com>
2024-06-15 11:46:49 -0500

    Pt 1 done

--- 2016/advent/src/days/_02/pt1.rs
+++ 2016/advent/src/days/_02/pt1.rs
@@ -1,9 +1,32 @@
 use crate::shared::io::read_lines_from_file;
 
 pub fn run() {
+    let lines = read_lines_from_file("src/days/_02/input.txt");
 
-    let lines = read_lines_from_file("src/days/_02/test.txt");
-    let line = lines.get(0).unwrap();
+    let keypad = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ];
 
-    println!("Money moves: {}", line);
+    let mut x = 1_i32;
+    let mut y = 1_i32;
+    let mut keycode = String::from("");
+
+    for line in lines {
+        for char in line.chars() {
+            match char {
+                'R' => x = (x+1).min(2),
+                'L' => x = (x-1).max(0),
+                'U' => y = (y-1).max(0),
+                'D' => y = (y+1).min(2),
+                c @ _ => panic!("Unknown char in input: '{}'", c)
+            }
+        }
+
+        keycode.push_str(&keypad[y as usize][x as usize].to_string());
+    }
+
+    println!("Keycode: {}", keycode);
+
 }
