21f9f3a6acc6b86201c0f048f38bfa9610b7cf09
Jacob Muchow <jacobamuchow@gmail.com>
2024-06-14 21:13:32 -0500

    Read from file (baby steps)

--- 2016/advent/src/main.rs
+++ 2016/advent/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello there");
+
+    let path = Path::new("src/input.txt");
+    println!("Going to read from file... '{}'", path.display());
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("Error opening file '{}': {}", path.display(), why),
+        Ok(file) => file,
+    };
+
+    let mut contents = String::new();
+    match file.read_to_string(&mut contents) {
+        Err(why) => panic!("Error reading data from file '{}', {}", path.display(), why),
+        Ok(_) => ()
+    }
+
+    println!("{}", contents);
 }
