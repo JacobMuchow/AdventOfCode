5fc92b71fd50f320d28cfddd52216a85533dbc5a
Jacob Muchow <jacobamuchow@gmail.com>
2024-06-14 22:23:01 -0500

    Work on file hierarchy

--- 2016/advent/src/main.rs
+++ 2016/advent/src/main.rs
@@ -1,16 +1,5 @@
-use std::fs::read_to_string;
-
-fn read_lines(path: &str) -> Vec<String> {
-    read_to_string(path)
-        .unwrap()
-        .lines()
-        .map(String::from)
-        .collect()
-}
+mod days;
 
 fn main() {
-    let lines = read_lines("src/input.txt");
-    for line in lines {
-        println!("{}", line);
-    }
+    days::run("src/test.txt");
 }
