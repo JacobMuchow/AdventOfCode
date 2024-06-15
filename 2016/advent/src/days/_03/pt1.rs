17864a2a2f8be4162be95fe599e106eca74137f0
Jacob Muchow <jacobamuchow@gmail.com>
2024-06-15 16:51:47 -0500

    Pt 1 done

--- 2016/advent/src/days/_03/pt1.rs
+++ 2016/advent/src/days/_03/pt1.rs
@@ -1,8 +1,32 @@
 use crate::shared::io::read_lines_from_file;
+use regex::Regex;
 
 pub fn run() {
-    let lines = read_lines_from_file("src/days/_03/test.txt");
+    let lines = read_lines_from_file("src/days/_03/input.txt");
+
+    let mut potential_triangles: Vec<(u32, u32, u32)> = Vec::new();
+    let re = Regex::new(r"([0-9]+)\s+([0-9]+)\s+([0-9]+)").unwrap();
+
     for line in lines {
-        println!("{}", line);
+        let caps = re.captures(&line).unwrap();
+
+        potential_triangles.push((
+            caps[1].parse().unwrap(),
+            caps[2].parse().unwrap(),
+            caps[3].parse().unwrap(),
+        ))
     }
+
+    let mut num_possible = 0_u32;
+
+    for sides in potential_triangles {
+        let (s1, s2, s3) = sides;
+        let max = s1.max(s2).max(s3);
+
+        if s1+s2 > max && s1+s3 > max && s2+s3 > max {
+            num_possible += 1;
+        }
+    }
+
+    println!("Num possible: {}", num_possible);
 }
