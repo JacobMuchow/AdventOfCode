6aeaf35c8d0bd15f0183cfebc2f1d2ec92279205
Jacob Muchow <jacobamuchow@gmail.com>
2024-06-15 00:37:06 -0500

    Pt 1 done

--- 2016/advent/src/days/_01/pt1.rs
+++ 2016/advent/src/days/_01/pt1.rs
@@ -1,6 +1,34 @@
 use std::fs::read_to_string;
 use regex::Regex;
 
+enum Direction {
+    North,
+    East,
+    South,
+    West
+}
+
+impl Direction {
+    fn right(&self) -> Direction {
+        match self {
+            North => East,
+            East => South,
+            South => West,
+            West => North
+        }
+    }
+    fn left(&self) -> Direction {
+        match self {
+            North => West,
+            West => South,
+            South => East,
+            East => North
+        }
+    }
+}
+
+use Direction::*;
+
 fn read_lines(path: &str) -> Vec<String> {
     read_to_string(path)
         .unwrap()
@@ -13,18 +41,41 @@ pub fn run() {
     let lines = read_lines("src/days/_01/input.txt");
     let line = lines.get(0).unwrap();
 
-    println!("{}", line);
-
+    // Parse directions
     let re = Regex::new(r"([LR])([0-9]+)").unwrap();
-
     let directions = re.captures_iter(line.as_str()).map(|caps| {
         let dir = caps.get(1).map(|m| m.as_str()).unwrap();
-        let dist = caps.get(2).map(|m| m.as_str()).unwrap();
-
+        let dist = caps.get(2).map(|m| m.as_str().parse::<i32>().unwrap()).unwrap();
         (dir, dist)
     });
 
-    for dir in directions {
-        println!("{}{}", dir.0, dir.1);
+    // Initialize position & direction
+    let mut x = 0;
+    let mut y = 0;
+    let mut facing = North;
+
+    // Handle all directions
+    for (dir, dist) in directions {
+        // Turn to new direction.
+        if dir == "R" {
+            facing = facing.right();
+        } else if dir == "L" {
+            facing = facing.left();
+        } else {
+            panic!("Unexpected dir: '{}'", dir);
+        }
+
+        // Move forward
+        match facing {
+            North => y += dist,
+            South => y -= dist,
+            East => x += dist,
+            West => x -= dist,
+        }
     }
+
+    println!("Final pos: {}, {}", x, y);
+
+    let shortest_path = x.abs() + y.abs();
+    println!("Shortest path: {}", shortest_path);
 }
