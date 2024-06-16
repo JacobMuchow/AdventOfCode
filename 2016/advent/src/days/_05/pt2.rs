f1597c5c6e4c9de09deab180f35895604e6a78ca
Jacob Muchow <jacobamuchow@gmail.com>
2024-06-16 14:10:01 -0500

    setup pt 5

---
+++ 2016/advent/src/days/_05/pt2.rs
@@ -0,0 +1,12 @@
+#![allow(dead_code)]
+
+use crate::shared::io::read_lines_from_file;
+// use regex::Regex;
+
+pub fn run() {
+    let lines = read_lines_from_file("src/days/_04/input.txt");
+
+    for line in lines {
+        println!("{}", line);
+    }
+}
