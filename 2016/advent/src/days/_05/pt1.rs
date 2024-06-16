dbd43c11aeaa888fcf317778243505e6128e6b70
Jacob Muchow <jacobamuchow@gmail.com>
2024-06-16 14:35:07 -0500

    Pt 1 done

--- 2016/advent/src/days/_05/pt1.rs
+++ 2016/advent/src/days/_05/pt1.rs
@@ -1,12 +1,29 @@
 // #![allow(dead_code)]
 
-use crate::shared::io::read_lines_from_file;
-// use regex::Regex;
+use md5;
+
+fn md5_hash(str: String) -> String {
+    let digest = md5::compute(str);
+    return format!("{:x}", digest);
+}
 
 pub fn run() {
-    let lines = read_lines_from_file("src/days/_05/test.txt");
+    // let door_id = String::from("abc");
+    let door_id = String::from("wtnhxymk");
+    let mut index = 0_u32;
 
-    for line in lines {
-        println!("{}", line);
+    let mut password = String::from("");
+
+    while password.len() < 8 {
+        let val = format!("{}{}", door_id, index);
+        let hash = md5_hash(val);
+
+        if hash.starts_with("00000") {
+            password.push(hash.chars().nth(5).unwrap());
+        }
+
+        index += 1;
     }
+
+    println!("Password: {}", password);
 }
