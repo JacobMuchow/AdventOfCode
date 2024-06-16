bd9034745dff5a08f8cdd5818e2e572cbc9aa6d1
Jacob Muchow <jacobamuchow@gmail.com>
2024-06-16 17:39:45 -0500

    pt 2 done

--- 2016/advent/src/days/_05/pt2.rs
+++ 2016/advent/src/days/_05/pt2.rs
@@ -1,12 +1,46 @@
-#![allow(dead_code)]
+// #![allow(dead_code)]
 
-use crate::shared::io::read_lines_from_file;
-// use regex::Regex;
+use md5;
+
+fn md5_hash(str: String) -> String {
+    let digest = md5::compute(str);
+    return format!("{:x}", digest);
+}
 
 pub fn run() {
-    let lines = read_lines_from_file("src/days/_04/input.txt");
+    // let door_id = String::from("abc");
+    let door_id = String::from("wtnhxymk");
+    let mut index = -1;
 
-    for line in lines {
-        println!("{}", line);
+    let mut password = String::from("........");
+    let mut filled = 0;
+
+    while filled < 8 {
+        index += 1;
+
+        let val = format!("{}{}", door_id, index);
+        let hash = md5_hash(val);
+
+        if hash.starts_with("00000") {
+            let pos_raw = hash.chars().nth(5).unwrap();
+            if pos_raw < '0' || pos_raw > '7' {
+                continue;
+            }
+
+            let pos: usize = pos_raw.to_string().parse().unwrap();
+            let mut chars: Vec<char> = password.chars().collect();
+
+            if chars[pos] != '.' {
+                continue;
+            }
+
+            let value = hash.chars().nth(6).unwrap();
+
+            chars[pos] = value;
+            password = chars.into_iter().collect();
+            filled += 1;
+        }
     }
+
+    println!("Password: {}", password);
 }
