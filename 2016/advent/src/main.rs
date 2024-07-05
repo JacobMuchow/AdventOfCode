ffbf315220003c96a44bd13a3cafc2fd7f490860
Jacob Muchow <jacobamuchow@gmail.com>
2024-07-05 12:42:05 -0500

    Added a benchmark wrapper.

--- 2016/advent/src/main.rs
+++ 2016/advent/src/main.rs
@@ -1,6 +1,17 @@
+use std::time::Instant;
+
 mod days;
 mod shared;
 
-fn main() {
+fn run_solution() {
     days::_11::pt1::run();
+}
+
+fn main() {
+    println!("Running solution...");
+    let start = Instant::now();
+    run_solution();
+    
+    let duration = start.elapsed();
+    println!("Solution ran in {:?}", duration);
 }
