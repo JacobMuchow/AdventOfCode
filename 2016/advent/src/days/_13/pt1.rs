7ae257ca21a0a879a8b3a0ec36aa65f3f3588dfd
Jacob Muchow <jacobamuchow@gmail.com>
2024-07-05 18:48:43 -0500

    Working is_wall() function

--- 2016/advent/src/days/_13/pt1.rs
+++ 2016/advent/src/days/_13/pt1.rs
@@ -2,9 +2,38 @@
 
 use crate::shared::io::read_lines_from_file;
 
+fn is_wall(x: usize, y: usize, fave_num: usize) -> bool {
+    let sum = (x*x) + (3*x) + (2*x*y) + y + (y*y) + fave_num;
+    let binary = format!("{:b}", sum);
+
+    let mut count = 0;
+    for c in binary.chars() {
+        if c == '1' {
+            count += 1;
+        }
+    }
+
+    count % 2 == 1
+}
+
 pub fn run() {
-    let lines = read_lines_from_file("src/days/_07/input.txt");
-    for line in lines {
-        println!("{}", line);
+    let fave_num: usize = 10;
+    let mut grid: Vec<Vec<char>> = vec![vec!['.'; 10]; 10];
+
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            grid[y][x] = if is_wall(x, y, fave_num) {
+                '#'
+            } else {
+                '.'
+            }
+        }
+    }
+
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            print!("{}", grid[y][x]);
+        }
+        println!();
     }
 }
