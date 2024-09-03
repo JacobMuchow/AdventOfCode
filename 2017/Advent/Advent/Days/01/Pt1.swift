68631bfe1a93a823d51a4d9760541b481f34c1f8
Jacob Muchow <jacobamuchow@gmail.com>
2024-09-03 15:32:34 -0700

    Move solution to its own file

---
+++ 2017/Advent/Advent/Days/01/Pt1.swift
@@ -0,0 +1,37 @@
+//
+//  Pt1.swift
+//  Advent
+//
+//  Created by Jacob Muchow on 9/3/24.
+//
+
+import Foundation
+
+class Day01Pt1 {
+    static func run() {
+        let lines = IOUtils.readLinesFromFile("input.txt")
+
+        let sequence = lines[0]
+        var total = 0
+
+        var indexA = sequence.startIndex
+
+        while indexA != sequence.endIndex {
+            var indexB = sequence.index(after: indexA)
+            if indexB == sequence.endIndex {
+                indexB = sequence.startIndex
+            }
+
+            if sequence[indexA] == sequence[indexB] {
+                let val = sequence[indexA]
+                let numVal = Int("\(val)", radix: 10)!
+
+                total += numVal
+            }
+
+            indexA = sequence.index(after: indexA)
+        }
+
+        print("Total: \(total)")
+    }
+}
