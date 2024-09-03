ad27f677e779097d7cba65032feca3220ffa5041
Jacob Muchow <jacobamuchow@gmail.com>
2024-09-02 21:22:39 -0700

    BFS to find next possible spots

--- 2016/advent/src/days/_24/pt1.rs
+++ 2016/advent/src/days/_24/pt1.rs
@@ -1,11 +1,21 @@
 #![allow(dead_code)]
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet, VecDeque};
 
-use crate::shared::io::read_lines_from_file;
+use map_macro::{hash_set, vec_deque};
+
+use crate::{days::_11::models::GameState, shared::io::read_lines_from_file};
 
 type Grid = Vec<Vec<char>>;
+type Hotspots = HashMap<char, Pos2d>;
 type Pos2d = (usize, usize);
+
+struct MapState {
+    grid: Grid,
+    hotspots: Hotspots,
+    pos: Pos2d,
+    num_steps: u32,
+}
 
 fn parse_input(filename: &str) -> Grid {
     let lines = read_lines_from_file(filename);
@@ -35,18 +45,74 @@ fn print_grid(grid: &Grid) {
     }
 }
 
+fn next_possible_spots(state: &MapState) -> Vec<Pos2d> {
+    let mut spots = Vec::<Pos2d>::new();
+
+    let mut visited: HashSet<Pos2d> = hash_set! {};
+    let mut queue: VecDeque<Pos2d> = vec_deque![state.pos];
+
+    while !queue.is_empty() {
+        let pos = queue.pop_front().unwrap();
+        let (x, y) = pos;
+        let symbol = state.grid[y][x];
+
+        // Ignore walls
+        if symbol == '#' {
+            continue;
+        }
+
+        // Do not re-visit
+        if visited.contains(&pos) {
+            continue;
+        }
+        visited.insert(pos);
+
+        // Found hot spot, add to viable options.
+        if symbol.is_digit(10) {
+            spots.push(pos);
+            continue;
+        }
+
+        // This is a empty space, so we must enqueue next possible options.
+        if x > 0 {
+            queue.push_back((x - 1, y));
+        }
+        if x < state.grid[0].len() {
+            queue.push_back((x + 1, y));
+        }
+        if y > 0 {
+            queue.push_back((x, y - 1));
+        }
+        if y < state.grid.len() {
+            queue.push_back((x, y + 1));
+        }
+    }
+
+    return spots;
+}
+
 pub fn run() {
-    let grid = parse_input("src/days/_24/input.txt");
+    let mut grid = parse_input("src/days/_24/test.txt");
     print_grid(&grid);
 
     let hotspots = get_hotspots(&grid);
-    let digits = hotspots.keys().collect::<Vec<&char>>();
-
+    let total_hotspots = hotspots.len();
     println!("Hotspots: {:?}", hotspots);
-    println!("Digits: {:?}", digits);
 
-    let mut unvisited = digits.clone();
-    let mut pos = hotspots[&'0'];
+    let mut pos = hotspots[&'3'];
 
     println!("Starting at {:?}", pos);
+    println!("Total: {}", total_hotspots);
+
+    grid[pos.1][pos.0] = '.';
+
+    let state = MapState {
+        grid,
+        hotspots,
+        pos,
+        num_steps: 0,
+    };
+
+    let next_spots = next_possible_spots(&state);
+    println!("Next spots: {:?}", next_spots);
 }
