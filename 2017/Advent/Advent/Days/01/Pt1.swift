4fa9d1821718a6dd74419b32685cecaeadf77c4e
Jacob Muchow <jacobamuchow@gmail.com>
2024-09-03 15:56:36 -0700

    Me like this better

--- 2017/Advent/Advent/Days/01/Pt1.swift
+++ 2017/Advent/Advent/Days/01/Pt1.swift
@@ -13,23 +13,19 @@ class Day01Pt1 {
 
         let sequence = lines[0]
         var total = 0
-
-        var indexA = sequence.startIndex
-
-        while indexA != sequence.endIndex {
-            var indexB = sequence.index(after: indexA)
-            if indexB == sequence.endIndex {
-                indexB = sequence.startIndex
-            }
-
-            if sequence[indexA] == sequence[indexB] {
-                let val = sequence[indexA]
+        
+        for i in 0..<sequence.count {
+            let j = (i+1) % sequence.count
+            
+            let indexI = sequence.index(sequence.startIndex, offsetBy: i)
+            let indexJ = sequence.index(sequence.startIndex, offsetBy: j)
+            
+            if sequence[indexI] == sequence[indexJ] {
+                let val = sequence[indexI]
                 let numVal = Int("\(val)", radix: 10)!
 
                 total += numVal
             }
-
-            indexA = sequence.index(after: indexA)
         }
 
         print("Total: \(total)")
