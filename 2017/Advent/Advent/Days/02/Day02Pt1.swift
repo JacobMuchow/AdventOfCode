d6c33dd1f4aa095e342ec20327bc1bc0c4ceabb3
Jacob Muchow <jacobamuchow@gmail.com>
2024-09-03 16:48:25 -0700

    day 02 pt 1 done

--- 2017/Advent/Advent/Days/02/Day02Pt1.swift
+++ 2017/Advent/Advent/Days/02/Day02Pt1.swift
@@ -9,11 +9,31 @@ import Foundation
 
 class Day02Pt1 {
     static func run() {
-        let lines = IOUtils.readLinesFromFile("day02_test.txt")
+        let lines = IOUtils.readLinesFromFile("day02_input.txt")
+        
+        var total = 0
 
         for line in lines {
-            print(line)
+            let tokens = line.components(separatedBy: .whitespaces).filter { !$0.isEmpty }
+            
+            var low = Int.max
+            var high = 0
+            
+            for token in tokens {
+                guard let val = Int(token, radix: 10) else {
+                    print("Failed to parse number from token: \(token)")
+                    fatalError()
+                }
+                
+                low = min(low, val)
+                high = max(high, val)
+            }
+            
+            let diff = high - low
+            print("Diff: \(diff)")
+            total += diff
         }
-        print("Done.")
+        
+        print("Checksum: \(total)")
     }
 }
