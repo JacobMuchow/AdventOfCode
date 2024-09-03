ec5c82ca71aed771ab0b2f75f84d4dc1760a8fc6
Jacob Muchow <jacobamuchow@gmail.com>
2024-09-03 13:56:47 -0700

    Read string content from txt file

--- 2017/Advent/Advent/main.swift
+++ 2017/Advent/Advent/main.swift
@@ -9,3 +9,17 @@ import Foundation
 
 print("Hello, World!")
 
+let currentDir = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+
+print("Current dir: \(currentDir.absoluteString)")
+
+let fileURL = currentDir.appendingPathComponent("input.txt")
+
+print("File URL: \(fileURL)")
+
+do {
+    let content = try String(contentsOf: fileURL, encoding: .utf8)
+    print(content)
+} catch {
+    print("Error reading file: \(error.localizedDescription)")
+}
