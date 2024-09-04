49f4b25d15da62d0f8af9e9754549ab55318d9de
Jacob Muchow <jacobamuchow@gmail.com>
2024-09-04 15:51:56 -0700

    Pt 1 done

--- 2017/Advent/Advent/Days/05/Day05Pt1.swift
+++ 2017/Advent/Advent/Days/05/Day05Pt1.swift
@@ -2,9 +2,19 @@ import Foundation
 
 class Day05Pt1 {
     static func run() {
-        let lines = IOUtils.readLinesFromFile("day05_test.txt")
-        for line in lines {
-            print(line)
+        let lines = IOUtils.readLinesFromFile("day05_input.txt")
+        
+        var jumps = lines.map { Int($0, radix: 10)! }
+        var lineNum = 0
+        var numSteps = 0
+        
+        while lineNum < jumps.count {
+            let newLine = lineNum + jumps[lineNum]
+            jumps[lineNum] += 1
+            lineNum = newLine
+            numSteps += 1
         }
+        
+        print("Maze exited. Num steps: \(numSteps)")
     }
 }
