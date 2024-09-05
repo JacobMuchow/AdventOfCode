e3af430ff3f4a3b75229b50039c08ccc67fdd0c8
Jacob Muchow <jacobamuchow@gmail.com>
2024-09-04 22:21:50 -0700

    set up day 07

---
+++ 2017/Advent/Advent/Days/07/Day07Pt1.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class Day07Pt1 {
+    static func run() {
+        let lines = IOUtils.readLinesFromFile("day07_test.txt")
+        for line in lines {
+            print(line)
+        }
+    }
+}
--- 2017/Advent/Advent/main.swift
+++ 2017/Advent/Advent/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 func runSolution() {
-    Day06Pt2.run()
+    Day07Pt1.run()
 }
 
 
