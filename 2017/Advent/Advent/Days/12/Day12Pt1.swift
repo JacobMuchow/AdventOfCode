69a40f02a058eefbf6010fb8e0ffe2e9f300e658
Jacob Muchow <jacobamuchow@gmail.com>
2024-11-29 17:30:19 -0600

    Set up day 12

---
+++ 2017/Advent/Advent/Days/12/Day12Pt1.swift
@@ -0,0 +1,17 @@
+//
+//  Day12Pt1.swift
+//  Advent
+//
+//  Created by Jacob Muchow on 11/29/24.
+//
+
+import Foundation
+
+class Day12Pt1 {
+    static func run() {
+        let lines = IOUtils.readLinesFromFile("day12_input.txt")
+        for line in lines {
+            print(line)
+        }
+    }
+}
