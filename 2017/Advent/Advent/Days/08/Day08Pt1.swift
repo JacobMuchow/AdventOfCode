cbcc4fef5e38a45c3ab0ab47b01f37e4ab807cee
Jacob Muchow <jacobamuchow@gmail.com>
2024-11-28 22:41:10 -0600

    Set up Day 08

---
+++ 2017/Advent/Advent/Days/08/Day08Pt1.swift
@@ -0,0 +1,18 @@
+//
+//  Day08Pt1.swift
+//  Advent
+//
+//  Created by Jacob Muchow on 11/28/24.
+//
+
+import Foundation
+
+class Day08Pt1 {
+    static func run() {
+        let lines = IOUtils.readLinesFromFile("day08_test.txt")
+
+        for line in lines {
+            print(line)
+        }
+    }
+}
