ced07f33ef80bf411fefabbd25b6c66c651dd413
Jacob Muchow <jacobamuchow@gmail.com>
2024-11-28 23:06:19 -0600

    Scaffold day 09

---
+++ 2017/Advent/Advent/Days/09/Day09Pt1.swift
@@ -0,0 +1,16 @@
+//
+//  Day09Pt1.swift
+//  Advent
+//
+//  Created by Jacob Muchow on 11/28/24.
+//
+
+import Foundation
+
+class Day09Pt1 {
+    static func run() {
+        let lines = IOUtils.readLinesFromFile("day09_input.txt")
+        let input = lines[0]
+        print("Input: \(input)")
+    }
+}
