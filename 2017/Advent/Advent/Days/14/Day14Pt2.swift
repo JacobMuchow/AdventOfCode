13739fe7217c047d7364ed266bac1b162d9960f5
Jacob Muchow <jacobamuchow@gmail.com>
2024-11-30 12:49:54 -0600

    pt 2 done

--- 2017/Advent/Advent/Days/14/Day14Pt2.swift
+++ 2017/Advent/Advent/Days/14/Day14Pt2.swift
@@ -9,21 +9,69 @@ import Foundation
 
 class Day14Pt2 {
     static func run() {
-        let lines = IOUtils.readLinesFromFile("day14_test.txt")
+        let lines = IOUtils.readLinesFromFile("day14_input.txt")
         let input = lines[0]
         
         let grid = parseGrid(input)
+        let regionCount = countRegions(grid)
         
-        var count = 0
-        for i in 0..<128 {
-            for j in 0..<128 {
-                if grid[i][j] == "#" {
-                    count += 1
+        print("Region count: \(regionCount)")
+    }
+    
+    static func countRegions(_ grid: [[String]]) -> Int {
+        var posToRegionMap: [String: Int] = [:]
+        var regionCount: Int = 0
+        
+        for y in 0..<128 {
+            for x in 0..<128 {
+                if grid[y][x] == "." { continue }
+                if posToRegionMap[keyFor(pos: (x, y))] == nil {
+                    let region = discoverRegion(grid: grid, startPos: (x, y))
+                    regionCount += 1
+                    region.forEach { posToRegionMap[keyFor(pos: $0)] = regionCount }
                 }
             }
         }
         
-        print("Count: \(count)")
+        return regionCount
+    }
+    
+    // Given a starting pos, search for all connected neighbors and return a list of coords for the entire region.
+    static func discoverRegion(grid: [[String]], startPos: (Int, Int)) -> [(Int, Int)] {
+        var visited: Set<String> = []
+        var region: [(Int, Int)] = [startPos]
+        var queue: [(Int, Int)] = [startPos]
+        
+        while !queue.isEmpty {
+            let (x, y) = queue.removeFirst()
+            
+            // Ignore invalid positions.
+            if (x < 0 || y < 0 || x >= grid[0].count || y >= grid.count) {
+                continue
+            }
+            
+            // Skip already visited positions.
+            let key = keyFor(pos: (x, y))
+            if (visited.contains(key)) {
+                continue
+            }
+            visited.insert(key)
+            
+            // Add pos to region, and enqueue neighbors.
+            if grid[y][x] == "#" {
+                region.append((x, y))
+                queue.append((x+1, y))
+                queue.append((x-1, y))
+                queue.append((x, y+1))
+                queue.append((x, y-1))
+            }
+        }
+        
+        return region
+    }
+    
+    static func keyFor(pos: (Int, Int)) -> String {
+        return "\(pos.0),\(pos.1)"
     }
     
     static func parseGrid(_ input: String) -> [[String]] {
