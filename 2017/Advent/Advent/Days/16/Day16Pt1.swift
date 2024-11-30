f9542b5ee447b39c73aaf81447f6bccd3aad4b95
Jacob Muchow <jacobamuchow@gmail.com>
2024-11-30 13:28:53 -0600

    Setup day16 pt 1

---
+++ 2017/Advent/Advent/Days/16/Day16Pt1.swift
@@ -0,0 +1,16 @@
+//
+//  Day16Pt1.swift
+//  Advent
+//
+//  Created by Jacob Muchow on 11/30/24.
+//
+
+import Foundation
+
+class Day16Pt1 {
+    static func run() {
+        let lines = IOUtils.readLinesFromFile("day16_input.txt")
+        let input = lines[0]
+        print("Input: \(input)")
+    }
+}
