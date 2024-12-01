f554966a31a11c015f1965fab96ec215431be496
Jacob Muchow <jacobamuchow@gmail.com>
2024-11-30 22:50:15 -0600

    day 17 pt 1 done

--- 2017/Advent/Advent/Days/17/Day17Pt1.swift
+++ 2017/Advent/Advent/Days/17/Day17Pt1.swift
@@ -10,7 +10,31 @@ import Foundation
 class Day17Pt1 {
     static func run() {
         let lines = IOUtils.readLinesFromFile("day17_input.txt")
-        let input = lines[0]
-        print("Input: \(input)")
+        let stepCount = Int(lines[0])!
+        
+        print("Step count: \(stepCount)")
+        
+        var buffer = [0]
+        var currentPos = 0
+        
+        for i in 1...2017 {
+            // Step forward (with some optimizations & circular list logic).
+            var stepsLeft = stepCount
+            var disToEnd = buffer.count - 1 - currentPos
+            
+            if stepsLeft > disToEnd {
+                stepsLeft -= (disToEnd+1)
+                currentPos = 0
+            }
+            
+            stepsLeft = stepsLeft % buffer.count
+            currentPos += stepsLeft
+            
+            buffer.insert(i, at: currentPos+1)
+            currentPos += 1
+        }
+        
+        let valueAfter = buffer[currentPos+1]
+        print("Value after last insert: \(valueAfter)")
     }
 }
