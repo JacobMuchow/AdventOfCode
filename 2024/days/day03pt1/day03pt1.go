18ffe76b4476f6bfbc617485fb726473aecdfea1
Jacob Muchow <jacobamuchow@gmail.com>
2024-12-03 00:48:39 -0800

    pt 1 done

--- 2024/days/day03pt1/day03pt1.go
+++ 2024/days/day03pt1/day03pt1.go
@@ -2,6 +2,7 @@ package day03pt1
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/JacobMuchow/AdventOfCode/2024/utils"
 )
@@ -9,8 +10,26 @@ import (
 func Run() {
 	lines := utils.ReadLinesFromFile("resources/day03_input.txt")
 
-	// Parse reports into list.
-	for _, line := range lines {
-		fmt.Println(line)
+	regex, ok := regexp.Compile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	if ok != nil {
+		panic("Failed to compile regex")
 	}
+
+	sumTotal := 0
+
+	for _, line := range lines {
+		tokens := regex.FindAllStringSubmatch(line, -1)
+		if tokens == nil {
+			panic("Failed to match from line")
+		}
+
+		for _, token := range tokens {
+			a := utils.ParseInt(token[1])
+			b := utils.ParseInt(token[2])
+
+			sumTotal += a * b
+		}
+	}
+
+	fmt.Println("Sum total:", sumTotal)
 }
