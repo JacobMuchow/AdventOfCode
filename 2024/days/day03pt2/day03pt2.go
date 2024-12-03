4ef4c9ab4695de1614d3243992ba48470d089e6c
Jacob Muchow <jacobamuchow@gmail.com>
2024-12-03 00:57:05 -0800

    pt 2 done

--- 2024/days/day03pt2/day03pt2.go
+++ 2024/days/day03pt2/day03pt2.go
@@ -8,26 +8,36 @@ import (
 )
 
 func Run() {
-	lines := utils.ReadLinesFromFile("resources/day03_test.txt")
+	lines := utils.ReadLinesFromFile("resources/day03_input.txt")
 
-	regex, ok := regexp.Compile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	regex, ok := regexp.Compile(`mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)`)
 	if ok != nil {
 		panic("Failed to compile regex")
 	}
 
 	sumTotal := 0
+	mulEnabled := true
 
 	for _, line := range lines {
-		tokens := regex.FindAllStringSubmatch(line, -1)
-		if tokens == nil {
+		matches := regex.FindAllStringSubmatch(line, -1)
+		if matches == nil {
 			panic("Failed to match from line")
 		}
 
-		for _, token := range tokens {
-			a := utils.ParseInt(token[1])
-			b := utils.ParseInt(token[2])
+		for _, match := range matches {
+			switch match[0] {
+			case "do()":
+				mulEnabled = true
+			case "don't()":
+				mulEnabled = false
+			default:
+				if mulEnabled {
+					a := utils.ParseInt(match[1])
+					b := utils.ParseInt(match[2])
 
-			sumTotal += a * b
+					sumTotal += a * b
+				}
+			}
 		}
 	}
 
