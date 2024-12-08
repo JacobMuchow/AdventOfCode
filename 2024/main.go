8d8c25fc3898701c8dfaad70fce2982c7a9b0990
Jacob Muchow <jacobamuchow@gmail.com>
2024-12-08 00:55:21 -0800

    Setup day 8

--- 2024/main.go
+++ 2024/main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/JacobMuchow/AdventOfCode/2024/days/day07pt2"
+	"github.com/JacobMuchow/AdventOfCode/2024/days/day08pt1"
 )
 
 func main() {
 	start := time.Now()
-	day07pt2.Run()
+	day08pt1.Run()
 	time_taken := time.Since(start)
 	fmt.Printf("Time taken: %v\n", time_taken)
 }
