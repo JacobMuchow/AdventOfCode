9b9972c29bd9aba850c1bb48b5a50e08898edd24
Jacob Muchow <jacobamuchow@gmail.com>
2024-12-08 22:18:31 -0800

    set up day 09

---
+++ 2024/days/day09pt1/day09pt1.go
@@ -0,0 +1,14 @@
+package day09pt1
+
+import (
+	"fmt"
+
+	"github.com/JacobMuchow/AdventOfCode/2024/utils"
+)
+
+func Run() {
+	lines := utils.ReadLinesFromFile("resources/day09_test.txt")
+	input := lines[0]
+
+	fmt.Println(input)
+}
--- 2024/main.go
+++ 2024/main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/JacobMuchow/AdventOfCode/2024/days/day08pt2"
+	"github.com/JacobMuchow/AdventOfCode/2024/days/day09pt1"
 )
 
 func main() {
 	start := time.Now()
-	day08pt2.Run()
+	day09pt1.Run()
 	time_taken := time.Since(start)
 	fmt.Printf("Time taken: %v\n", time_taken)
 }
