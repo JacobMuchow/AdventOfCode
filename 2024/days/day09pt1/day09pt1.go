11269958e7577f0ae552ec616ab01b43b85f7f43
Jacob Muchow <jacobamuchow@gmail.com>
2024-12-13 09:34:32 -0800

    save wip

--- 2024/days/day09pt1/day09pt1.go
+++ 2024/days/day09pt1/day09pt1.go
@@ -1,14 +1,59 @@
 package day09pt1
 
 import (
-	"fmt"
-
 	"github.com/JacobMuchow/AdventOfCode/2024/utils"
+	"github.com/JacobMuchow/AdventOfCode/2024/utils/arrays"
 )
+
+type Segment struct {
+	fileId int
+	size   int
+}
 
 func Run() {
 	lines := utils.ReadLinesFromFile("resources/day09_test.txt")
 	input := lines[0]
 
-	fmt.Println(input)
+	segments := parseSegments(input)
+
+	for i := 0; i < len(segments); {
+		if segments[i].fileId != -1 {
+			i +=1
+			continue
+		}
+
+
+	}
+}
+
+func fillEmptySegment(segments []Segment, pos int) []Segment {
+	emptySeg := segments[pos]
+
+	movedSegs := make([]Segment, 0)
+	for i := pos+1; i < len(segments); i++ {
+		if 
+	}
+}
+
+func parseSegments(input string) []Segment {
+	runes := []rune(input)
+
+	segments := make([]Segment, len(runes))
+	isEmpty := false
+
+	for i, char := range runes {
+		fileId := -1
+		if !isEmpty {
+			fileId = i / 2
+		}
+
+		segments[i] = Segment{
+			fileId: fileId,
+			size:   utils.ParseInt(string(char)),
+		}
+
+		isEmpty = !isEmpty
+	}
+
+	return segments
 }
