b44273b1553856ca74e144fbd100fafd4c0e8149
Jacob Muchow <jacobamuchow@gmail.com>
2024-12-15 22:22:33 -0600

    Double-pointer LL traversal implementation

--- 2024/days/day09pt1/day09pt1.go
+++ 2024/days/day09pt1/day09pt1.go
@@ -7,17 +7,14 @@ import (
 )
 
 type LinkedList struct {
-	head *Segment
-	tail *Segment
+	head *Node
+	tail *Node
 }
 
-type Segment struct {
-	fileId int
-	size   int
-	empty  bool
-
-	prev *Segment
-	next *Segment
+type Node struct {
+	value int
+	prev  *Node
+	next  *Node
 }
 
 func Run() {
@@ -26,31 +23,63 @@ func Run() {
 
 	list := parseSegments(input)
 	visualize(list)
-	// optimize(list)
+	optimize(list)
 
-	listMove(list, list.tail.prev, list.head.next)
+	// listMove(list.tail.prev, list.head.next)
 	visualize(list)
 }
 
 func optimize(list *LinkedList) {
-	for cur := list.head.next; cur != list.tail; cur = cur.next {
-		if cur.empty {
-			filled := fillEmpty(list, cur)
-			if !filled {
-				break
+	left := list.head.next
+	right := list.tail.prev
+
+	for ; left != right; left = left.next {
+		if left.value == -1 {
+			for ; right != left; right = right.prev {
+				if right.value != -1 {
+					prev := right.prev
+					listMove(right, left)
+					listMove(left, prev)
+
+					swap := left
+					left = right
+					right = swap
+
+					visualize(list)
+					break
+				}
 			}
 		}
 	}
 }
 
-func fillEmpty(list *LinkedList, seg *Segment) bool {
+func fillEmpty(list *LinkedList, seg *Node) *Node {
 	for cur := list.tail.prev; cur != seg; cur = cur.prev {
-
+		if cur.value != -1 {
+			prev := cur.prev
+			listMove(cur, seg)
+			listMove(seg, prev)
+			return cur
+		}
 	}
-	return true
+	return nil
 }
 
-func listMove(list *LinkedList, seg *Segment, after *Segment) {
+func listInsert(value int, after *Node) *Node {
+	new := Node{value: value}
+
+	before := after.next
+
+	before.prev = &new
+	new.next = before
+
+	after.next = &new
+	new.prev = after
+
+	return &new
+}
+
+func listMove(seg *Node, after *Node) {
 	seg.prev.next = seg.next
 	seg.next.prev = seg.prev
 
@@ -64,43 +93,37 @@ func listMove(list *LinkedList, seg *Segment, after *Segment) {
 
 func visualize(list *LinkedList) {
 	for cur := list.head.next; cur != list.tail; cur = cur.next {
-		for i := 0; i < cur.size; i++ {
-			if cur.empty {
-				fmt.Printf(".")
-			} else {
-				fmt.Printf("%d", cur.fileId)
-			}
+		if cur.value == -1 {
+			fmt.Printf(".")
+		} else {
+			fmt.Printf("%d", cur.value)
 		}
 	}
 	fmt.Println()
 }
 
 func parseSegments(input string) *LinkedList {
-	runes := []rune(input)
+	head := &Node{}
+	tail := &Node{}
 
-	head := &Segment{fileId: -1, empty: true, size: 0}
-	tail := &Segment{fileId: -1, empty: true, size: 0}
+	head.next = tail
+	tail.prev = head
+
 	prev := head
-
 	isEmpty := false
 
-	for i, char := range runes {
-		fileId := -1
+	for i, char := range input {
+		size := utils.ParseInt(string(char))
+		value := -1
 		if !isEmpty {
-			fileId = i / 2
+			value = i / 2
 		}
 
-		segment := Segment{
-			fileId: fileId,
-			empty:  isEmpty,
-			size:   utils.ParseInt(string(char)),
+		for i := 0; i < size; i++ {
+			prev = listInsert(value, prev)
 		}
-
-		segment.prev = prev
-		prev.next = &segment
 
 		isEmpty = !isEmpty
-		prev = &segment
 	}
 
 	prev.next = tail
