eb7adbfd4a21b470bdef67428bcfaf606cf90773
Jacob Muchow <jacobamuchow@gmail.com>
2024-12-23 22:57:41 -0800

    setup day 10

---
+++ 2024/days/day10pt1/day10pt1.go
@@ -0,0 +1,15 @@
+package day10pt1
+
+import (
+	"fmt"
+
+	"github.com/JacobMuchow/AdventOfCode/2024/utils"
+)
+
+func Run() {
+	lines := utils.ReadLinesFromFile("resources/day10_input.txt")
+
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+}
