2e87a3529ec4283e4da0b70bdb4e87169d17de2b
Jacob Muchow <jacobamuchow@gmail.com>
2024-12-23 23:14:51 -0800

    parse input

--- 2024/days/day10pt1/day10pt1.go
+++ 2024/days/day10pt1/day10pt1.go
@@ -6,10 +6,42 @@ import (
 	"github.com/JacobMuchow/AdventOfCode/2024/utils"
 )
 
-func Run() {
-	lines := utils.ReadLinesFromFile("resources/day10_input.txt")
+type Grid = [][]int
 
-	for _, line := range lines {
-		fmt.Println(line)
+func Run() {
+	lines := utils.ReadLinesFromFile("resources/day10_test.txt")
+
+	grid := parseGrid(lines)
+	printGrid(grid)
+}
+
+func parseGrid(lines []string) *Grid {
+	grid := make(Grid, len(lines))
+
+	for y, line := range lines {
+		row := make([]int, len(line))
+		for x, char := range line {
+			if char == '.' {
+				row[x] = -1
+			} else {
+				row[x] = utils.ParseInt(string(char))
+			}
+		}
+		grid[y] = row
+	}
+
+	return &grid
+}
+
+func printGrid(grid *Grid) {
+	for _, row := range *grid {
+		for _, val := range row {
+			if val == -1 {
+				fmt.Printf(".")
+			} else {
+				fmt.Printf("%d", val)
+			}
+		}
+		fmt.Println()
 	}
 }
