2bd9ae60777286c8357e0bb71459a378de05028e
Jacob Muchow <jacobamuchow@gmail.com>
2025-12-02 23:20:09 -0800

    Pt 1 done

--- 2025/Advent2025/src/solutions/Day01Pt1Solution.java
+++ 2025/Advent2025/src/solutions/Day01Pt1Solution.java
@@ -3,14 +3,46 @@ package solutions;
 import utils.ResourceUtils;
 import utils.Runnable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day01Pt1Solution implements Runnable {
     @Override
     public void run() {
-        var content = ResourceUtils.readLines("resources/day01/test.txt");
+        var lines = ResourceUtils.readLines("resources/day01/input.txt");
 
-        System.out.println("File content:");
-        for (var line : content) {
-            System.out.println(line);
+        var dirs = new ArrayList<String>();
+        var counts = new ArrayList<Integer>();
+
+        for (var line : lines) {
+            dirs.add(line.substring(0, 1));
+            counts.add(Integer.parseInt(line.substring(1)));
         }
+
+        var zeroCount = 0;
+        var dial = 50;
+
+        for (var i = 0; i < dirs.size(); i++) {
+            var dir = dirs.get(i);
+            var ticks = counts.get(i) % 100;
+
+            if (dir.equals("R")) {
+                dial += ticks;
+                if (dial > 99) {
+                    dial -= 100;
+                }
+            } else {
+                dial -= ticks;
+                if (dial < 0) {
+                    dial += 100;
+                }
+            }
+
+            if (dial == 0) {
+                zeroCount++;
+            }
+        }
+
+        System.out.printf("Zero count: %d\n", zeroCount);
     }
 }
