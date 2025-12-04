3df1ff5a0285a775dae521227ddcc5c3d4e4f855
Jacob Muchow <jacobamuchow@gmail.com>
2025-12-04 00:04:21 -0800

    day 04 pt 1 done

--- 2025/Advent2025/src/solutions/Day04Pt1Solution.java
+++ 2025/Advent2025/src/solutions/Day04Pt1Solution.java
@@ -3,11 +3,47 @@ package solutions;
 import utils.ResourceUtils;
 import utils.Runnable;
 
+import java.util.List;
+
 public class Day04Pt1Solution implements Runnable {
     @Override
     public void run() {
-        var lines = ResourceUtils.readString("resources/day04/test.txt");
+        var lines = ResourceUtils.readLines("resources/day04/input.txt");
+        char[][] grid = lines.stream().map(String::toCharArray).toArray(char[][]::new);
 
-        System.out.println(lines);
+        var numAccessible = 0;
+
+        for (var y = 0; y < grid.length; y++) {
+            for (var x = 0; x < grid[y].length; x++) {
+                if (grid[y][x] != '@') continue;
+                if (isToiletPaperAccessible(grid, x, y)) {
+                    numAccessible++;
+                }
+            }
+        }
+
+        System.out.printf("Num accessible: %d\n", numAccessible);
+    }
+
+    private boolean isToiletPaperAccessible(char[][] grid, int x, int y) {
+        var neighborCount = 0;
+
+        if (x > 0) {
+            if (y > 0 && grid[y-1][x-1] == '@') neighborCount++;
+            if (grid[y][x-1] == '@') neighborCount++;
+            if (y < grid.length-1 && grid[y+1][x-1] == '@') neighborCount++;
+        }
+
+        if (y > 0 && grid[y-1][x] == '@') neighborCount++;
+        // Skip self
+        if (y < grid.length-1 && grid[y+1][x] == '@') neighborCount++;
+
+        if (x < grid[0].length-1) {
+            if (y > 0 && grid[y-1][x+1] == '@') neighborCount++;
+            if (grid[y][x+1] == '@') neighborCount++;
+            if (y < grid.length-1 && grid[y+1][x+1] == '@') neighborCount++;
+        }
+
+        return neighborCount < 4;
     }
 }
