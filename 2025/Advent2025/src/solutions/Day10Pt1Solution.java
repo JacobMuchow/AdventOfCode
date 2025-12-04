7817a7eae412d726eb62a5b80c7aaa47f57032ea
Jacob Muchow <jacobamuchow@gmail.com>
2025-12-04 00:22:55 -0800

    Generate empty solutions for all remaining days.

---
+++ 2025/Advent2025/src/solutions/Day10Pt1Solution.java
@@ -0,0 +1,13 @@
+package solutions;
+
+import utils.ResourceUtils;
+import utils.Runnable;
+
+public class Day10Pt1Solution implements Runnable {
+    @Override
+    public void run() {
+        var input = ResourceUtils.readString("resources/day10/test.txt");
+        System.out.println(input);
+    }
+}
+
---
+++ 2025/Advent2025/src/solutions/Day11Pt1Solution.java
@@ -0,0 +1,13 @@
+package solutions;
+
+import utils.ResourceUtils;
+import utils.Runnable;
+
+public class Day11Pt1Solution implements Runnable {
+    @Override
+    public void run() {
+        var input = ResourceUtils.readString("resources/day11/test.txt");
+        System.out.println(input);
+    }
+}
+
