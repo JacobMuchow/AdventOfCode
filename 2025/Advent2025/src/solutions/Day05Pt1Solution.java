cb07059f2c03ef410fea03c04cf3d96198194c53
Jacob Muchow <jacobamuchow@gmail.com>
2025-12-04 00:15:06 -0800

    Set up day 05

---
+++ 2025/Advent2025/src/solutions/Day05Pt1Solution.java
@@ -0,0 +1,12 @@
+package solutions;
+
+import utils.ResourceUtils;
+import utils.Runnable;
+
+public class Day05Pt1Solution implements Runnable {
+    @Override
+    public void run() {
+        var input = ResourceUtils.readString("resources/day05/test.txt");
+        System.out.println(input);
+    }
+}
