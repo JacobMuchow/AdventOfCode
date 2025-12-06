f9af93acfe1aaa2046a78c725d3660bc6c096016
Jacob Muchow <jacobamuchow@gmail.com>
2025-12-06 01:02:16 -0800

    2025 day 06 pt 1 done

--- 2025/Advent2025/src/Main.java
+++ 2025/Advent2025/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         var start = System.currentTimeMillis();
 
-        SolutionRunner.run(Solution.Day05Pt2);
+        SolutionRunner.run(Solution.Day06Pt1);
 
         var timeTaken = System.currentTimeMillis() - start;
         System.out.printf("Time taken: %dms\n", timeTaken);
--- 2025/Advent2025/src/solutions/Day06Pt1Solution.java
+++ 2025/Advent2025/src/solutions/Day06Pt1Solution.java
@@ -3,11 +3,47 @@ package solutions;
 import utils.ResourceUtils;
 import utils.Runnable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Day06Pt1Solution implements Runnable {
     @Override
     public void run() {
-        var input = ResourceUtils.readString("resources/day06/test.txt");
-        System.out.println(input);
+        var lines = ResourceUtils.readLines("resources/day06/input.txt");
+
+        List<long[]> operandRows = new ArrayList<>();
+        String[] operators = new String[] {};
+
+        for (var i = 0; i < lines.size(); i++) {
+            final var line = lines.get(i).trim();
+            final var tokens = line.split("\\s+");
+
+            // All but last row are operands
+            if (i < lines.size()-1) {
+                long[] operands = Arrays.stream(tokens).mapToLong(Long::parseLong).toArray();
+                operandRows.add(operands);
+            } else {
+                operators = tokens;
+            }
+        }
+
+        long grandTotal = 0L;
+        for (var i = 0; i < operators.length; i++) {
+            final String operator = operators[i];
+
+            long total = operator.equals("+") ? 0 : 1;
+            for (var j = 0; j < operandRows.size(); j++) {
+                if (operator.equals("+")) {
+                    total += operandRows.get(j)[i];
+                } else {
+                    total *= operandRows.get(j)[i];
+                }
+            }
+            grandTotal += total;
+        }
+
+        System.out.printf("Grand total: %d\n", grandTotal);
     }
 }
 
