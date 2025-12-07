cc2b7e6fda84a76586e21630d8298d0c3039e12f
Jacob Muchow <jacobamuchow@gmail.com>
2025-12-06 22:10:12 -0800

    got it down to 3 seconds....

--- 2017/Advent/Advent/Days/24/Day24Pt1.swift
+++ 2017/Advent/Advent/Days/24/Day24Pt1.swift
@@ -9,55 +9,52 @@ class Day24Pt1 {
     static func run() {
         let lines = IOUtils.readLinesFromFile("day24_input.txt")
         
-        var partPool: [Part] = []
+        var partPool: [Int: [Part]] = [:]
         for i in 0..<lines.count {
             let tokens = lines[i].split(separator: "/")
-            partPool.append(Part(
+            
+            let partFwd = Part(
                 id: i+1,
                 port1: Int(String(tokens[0]))!,
                 port2: Int(String(tokens[1]))!
-            ))
+            )
+            partPool[partFwd.port1, default: []].append(partFwd)
+            
+            let partRev = Part(
+                id: i+1,
+                port1: Int(String(tokens[1]))!,
+                port2: Int(String(tokens[0]))!
+            )
+            partPool[partRev.port1, default: []].append(partRev)
         }
         
         var bridge = [Part(id: 0, port1: 0, port2: 0)]
+        var usedParts = Set<Int>()
         
-        let maxStrength = tryParts(bridge: &bridge, partPool: &partPool)
+        let maxStrength = tryParts(bridge: &bridge, partPool: &partPool, usedParts: &usedParts)
         
         print("Max strength: \(maxStrength)")
     }
     
-    private static func tryParts(bridge: inout [Part], partPool: inout [Part]) -> Int {
+    private static func tryParts(bridge: inout [Part], partPool: inout [Int: [Part]], usedParts: inout Set<Int>) -> Int {
         var maxStrength = calcStrength(bridge: bridge)
-        var pinMatch = openPortPins(bridge: bridge)
+        let pinMatch = openPortPins(bridge: bridge)
         
-        // Try all parts forward.
-        for (i, part) in partPool.enumerated() {
-            if (part.port1 != pinMatch) { continue }
+        let possibleParts = partPool[pinMatch, default: []]
+        
+        for part in possibleParts {
+            if usedParts.contains(part.id) { continue }
             
-            partPool.remove(at: i)
             bridge.append(part)
+            usedParts.insert(part.id)
             
-            var strength = tryParts(bridge: &bridge, partPool: &partPool)
+            let strength = tryParts(bridge: &bridge, partPool: &partPool, usedParts: &usedParts)
             maxStrength = max(maxStrength, strength)
             
             bridge.removeLast()
-            partPool.insert(part, at: i)
+            usedParts.remove(part.id)
         }
-        
-        // Try all parts backward.
-        for (i, part) in partPool.enumerated() {
-            if (part.port2 != pinMatch) { continue }
-            
-            partPool.remove(at: i)
-            bridge.append(Part(id: part.id, port1: part.port2, port2: part.port1))
-            
-            var strength = tryParts(bridge: &bridge, partPool: &partPool)
-            maxStrength = max(maxStrength, strength)
-            
-            bridge.removeLast()
-            partPool.insert(part, at: i)
-        }
-        
+ 
         return maxStrength
     }
     
@@ -71,7 +68,7 @@ class Day24Pt1 {
         })
     }
     
-    struct Part {
+    struct Part: CustomStringConvertible {
         let id: Int
         let port1: Int
         let port2: Int
@@ -80,6 +77,10 @@ class Day24Pt1 {
             self.id = id
             self.port1 = port1
             self.port2 = port2
+        }
+        
+        var description: String {
+            "\(self.id)|\(self.port1)/\(self.port2)"
         }
     }
     
