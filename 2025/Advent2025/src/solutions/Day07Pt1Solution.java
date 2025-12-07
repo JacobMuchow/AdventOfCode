e991a059d646a15d4a3fa05027b1dc9e1b3e114b
Jacob Muchow <jacobamuchow@gmail.com>
2025-12-07 00:05:37 -0800

    2025 day 7 pt 1 done

--- 2025/Advent2025/src/solutions/Day07Pt1Solution.java
+++ 2025/Advent2025/src/solutions/Day07Pt1Solution.java
@@ -3,11 +3,43 @@ package solutions;
 import utils.ResourceUtils;
 import utils.Runnable;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Day07Pt1Solution implements Runnable {
     @Override
     public void run() {
-        var input = ResourceUtils.readString("resources/day07/test.txt");
-        System.out.println(input);
+        final var grid = ResourceUtils.readCharGrid("resources/day07/input.txt");
+
+        int start = findStartX(grid[0]);
+        System.out.printf("start x: %d\n", start);
+
+        var tachyonBeams = new HashSet<Integer>();
+        tachyonBeams.add(start);
+
+        var splitCount = 0;
+        for (var y = 2; y < grid.length; y += 2) {
+            var newBeams = new HashSet<Integer>();
+            for (var beam : tachyonBeams) {
+                if (grid[y][beam] == '^') {
+                    newBeams.add(beam-1);
+                    newBeams.add(beam+1);
+                    splitCount += 1;
+                } else {
+                    newBeams.add(beam);
+                }
+            }
+            tachyonBeams = newBeams;
+        }
+
+        System.out.printf("Split count: %d\n", splitCount);
+    }
+
+    private int findStartX(char[] topRow) {
+        for (var i = 0; i < topRow.length; i++) {
+            if (topRow[i] == 'S') return i;
+        }
+        throw new Error("Starting position not found");
     }
 }
 
