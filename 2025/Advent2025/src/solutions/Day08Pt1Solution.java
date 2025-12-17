165da2f291770153f6b7401d400adeddc852407e
Jacob Muchow <jacobamuchow@gmail.com>
2025-12-17 00:27:27 -0800

    Day 08 pt 1 done

--- 2025/Advent2025/src/solutions/Day08Pt1Solution.java
+++ 2025/Advent2025/src/solutions/Day08Pt1Solution.java
@@ -3,26 +3,63 @@ package solutions;
 import utils.ResourceUtils;
 import utils.Runnable;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Parameter;
+import java.sql.Array;
+import java.util.*;
 
 public class Day08Pt1Solution implements Runnable {
     @Override
     public void run() {
-        final var lines = ResourceUtils.readLines("resources/day08/test.txt");
-        final List<Vec3D> points = new ArrayList<>(lines.size());
-        final var pairAmount = 10;
+        final var lines = ResourceUtils.readLines("resources/day08/input.txt");
+        final var pairAmount = 1000;
 
-        for (var line : lines) {
-            final var tokens = line.split(",");
-            points.add(new Vec3D(
-                Long.parseLong(tokens[0]),
-                Long.parseLong(tokens[1]),
-                Long.parseLong(tokens[2])
-            ));
+        final List<Vec3D> points = this.parsePoints(lines);
+
+        final List<Pairing> nearestPairs = this.findNearestPairs(points, pairAmount);
+
+        // This will contains a map from "circuit IDs" to list of points in the circuit.
+        //Initially, each point ("junction box") is its own individual circuit.
+        final Map<String, List<Vec3D>> circuits = new HashMap<>(points.size());
+        // We will also have a mapping from point -> Circuit ID for reverse lookup.
+        final Map<String, String> circuitLookup = new HashMap<>(points.size());
+
+        for (var point : points) {
+            final var circuitID = this.vecHash(point);
+            circuits.put(circuitID, new ArrayList<>(List.of(point)));
+            circuitLookup.put(circuitID, circuitID);
         }
 
-        final List<Pairing> nearestPairs = new ArrayList<>(pairAmount);
+        // Now, for each pair we need to merge circuits.
+        for (var pair : nearestPairs) {
+            final var p1 = pair.p1;
+            final var p2 = pair.p2;
+
+            final var circuitId1 = circuitLookup.get(this.vecHash(p1));
+            final var circuitId2 = circuitLookup.get(this.vecHash(p2));
+
+            if (circuitId1.equals(circuitId2)) continue;
+
+            var c1 = circuits.get(circuitId1);
+            var c2 = circuits.get(circuitId2);
+            c1.addAll(c2);
+            circuits.remove(circuitId2);
+
+            for (var point : c2) {
+                circuitLookup.put(this.vecHash(point), circuitId1);
+            }
+        }
+
+        System.out.printf("Total circuits: %d\n", circuits.size());
+
+        final var sorted = new ArrayList<>(circuits.values());
+        sorted.sort(Comparator.comparingInt((List<?> l) -> l.size()).reversed());
+
+        final var product = sorted.get(0).size() * sorted.get(1).size() * sorted.get(2).size();
+        System.out.printf("Product: %d\n", product);
+    }
+
+    private List<Pairing> findNearestPairs(List<Vec3D> points, int limit) {
+        final List<Pairing> nearestPairs = new ArrayList<>(limit);
 
         for (int i = 0; i < points.size()-1; i++) {
             final var p1 = points.get(i);
@@ -30,7 +67,7 @@ public class Day08Pt1Solution implements Runnable {
                 var p2 = points.get(j);
                 var dist = Math.sqrt(Math.powExact((p2.x-p1.x), 2) + Math.powExact((p2.y-p1.y), 2) + Math.powExact((p2.z-p1.z), 2));
 
-                if (nearestPairs.size() < pairAmount) {
+                if (nearestPairs.size() < limit) {
                     this.insertPair(nearestPairs, new Pairing(p1, p2, dist));
                 } else {
                     final var lastPair = nearestPairs.getLast();
@@ -42,9 +79,7 @@ public class Day08Pt1Solution implements Runnable {
             }
         }
 
-        for (var pair : nearestPairs) {
-            System.out.printf("%d,%d,%d - %d,%d,%d\n", pair.p1.x, pair.p1.y, pair.p1.z, pair.p2.x, pair.p2.y, pair.p2.z);
-        }
+        return nearestPairs;
     }
 
     private void insertPair(List<Pairing> pairs, Pairing newPair) {
@@ -62,8 +97,35 @@ public class Day08Pt1Solution implements Runnable {
         }
     }
 
-    record Vec3D(long x, long y, long z) {}
+    private List<Vec3D> parsePoints(List<String> lines) {
+        final List<Vec3D> points = new ArrayList<>(lines.size());
+        for (var line : lines) {
+            final var tokens = line.split(",");
+            points.add(new Vec3D(
+                    Long.parseLong(tokens[0]),
+                    Long.parseLong(tokens[1]),
+                    Long.parseLong(tokens[2])
+            ));
+        }
+        return points;
+    }
 
-    record Pairing(Vec3D p1, Vec3D p2, double dist) {}
+    private String vecHash(Vec3D point) {
+        return String.format("%d,%d,%d", point.x, point.y, point.z);
+    }
+
+    record Vec3D(long x, long y, long z) {
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(x, y, z);
+//        }
+    }
+
+    record Pairing(Vec3D p1, Vec3D p2, double dist) {
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(p1, p2);
+//        }
+    }
 }
 
