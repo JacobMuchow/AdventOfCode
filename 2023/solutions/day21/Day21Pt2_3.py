9bfbb25254d86742b9b275d02f1bbffe241df97c
Jacob Muchow <jacobamuchow@gmail.com>
2026-08-14 23:29:51 -0700

    First try: correct on test inputs, but slow at step scale as predicted.

--- 2023/solutions/day21/Day21Pt2_3.py
+++ 2023/solutions/day21/Day21Pt2_3.py
@@ -4,18 +4,41 @@ from enum import Enum
 from solutions.solution import Solution
 from utils.files import FileUtils
 
-STEP_GOAL = 64
+STEP_GOAL = 5000
+# STEP_GOAL = 26501365
 
 @dataclass
-class Pos2D:
-    x: int
-    y: int
+class ChunkPos:
+    chunk_x: int
+    chunk_y: int
+    local_x: int
+    local_y: int
 
     def __str__(self):
-        return f"{self.x},{self.y}"
+        return f"{self.chunk_x},{self.chunk_y}:{self.local_x},{self.local_y}"
 
     def key(self) -> str:
-        return f"{self.x},{self.y}"
+        return self.__str__()
+
+    def above(self, grid: list[str]) -> "ChunkPos":
+        if self.local_y <= 0:
+            return ChunkPos(self.chunk_x, self.chunk_y-1, self.local_x, len(grid)-1)
+        return ChunkPos(self.chunk_x, self.chunk_y, self.local_x, self.local_y-1)
+
+    def below(self, grid: list[str]) -> "ChunkPos":
+        if self.local_y >= len(grid)-1:
+            return ChunkPos(self.chunk_x, self.chunk_y+1, self.local_x, 0)
+        return ChunkPos(self.chunk_x, self.chunk_y, self.local_x, self.local_y+1)
+
+    def left(self, grid: list[str]) -> "ChunkPos":
+        if self.local_x <= 0:
+            return ChunkPos(self.chunk_x-1, self.chunk_y, len(grid[0])-1, self.local_y)
+        return ChunkPos(self.chunk_x, self.chunk_y, self.local_x-1, self.local_y)
+
+    def right(self, grid: list[str]) -> "ChunkPos":
+        if self.local_x >= len(grid[0])-1:
+            return ChunkPos(self.chunk_x+1, self.chunk_y, 0, self.local_y)
+        return ChunkPos(self.chunk_x, self.chunk_y, self.local_x+1, self.local_y)
 
 class Polarity(Enum):
     Even = 0
@@ -33,7 +56,7 @@ class Polarity(Enum):
 
 @dataclass
 class QueueItem:
-    pos: Pos2D
+    pos: ChunkPos
     polarity: Polarity
     step: int
 
@@ -41,15 +64,16 @@ class Day21Pt2_3Solution(Solution):
     grid: list[str]
 
     def run(self) -> None:
-        self.grid = FileUtils.read_lines('resources/day21/input.txt')
+        self.grid = FileUtils.read_lines('resources/day21/test.txt')
         for line in self.grid:
             print(line)
 
         start = self._find_start()
         print(f"Start: {start}")
+        print(f"Size: {len(self.grid[0])}x{len(self.grid)}")
 
         # Assign start '.' value so later comparisons are easier
-        self.grid[start.y] = self.grid[start.y].replace('S', '.', 1)
+        self.grid[start.local_y] = self.grid[start.local_y].replace('S', '.', 1)
 
         # Set up optimized search queue
         queue: list[QueueItem] = [QueueItem(start, Polarity.Even, step=0)]
@@ -63,9 +87,7 @@ class Day21Pt2_3Solution(Solution):
             pos = item.pos
 
             # Validate pos
-            if pos.y < 0 or pos.y >= len(self.grid): continue
-            if pos.x < 0 or pos.x >= len(self.grid[0]): continue
-            if self.grid[pos.y][pos.x] == '#': continue
+            if self.grid[pos.local_y][pos.local_x] == '#': continue
 
             # Skip seen, else add to seen
             if pos.key() in seen: continue
@@ -86,18 +108,18 @@ class Day21Pt2_3Solution(Solution):
             next_polarity = item.polarity.flip()
             next_step = item.step + 1
 
-            queue.append(QueueItem(Pos2D(pos.x-1, pos.y), next_polarity, next_step))
-            queue.append(QueueItem(Pos2D(pos.x+1, pos.y), next_polarity, next_step))
-            queue.append(QueueItem(Pos2D(pos.x, pos.y-1), next_polarity, next_step))
-            queue.append(QueueItem(Pos2D(pos.x, pos.y+1), next_polarity, next_step))
+            queue.append(QueueItem(pos.above(self.grid), next_polarity, next_step))
+            queue.append(QueueItem(pos.below(self.grid), next_polarity, next_step))
+            queue.append(QueueItem(pos.left(self.grid), next_polarity, next_step))
+            queue.append(QueueItem(pos.right(self.grid), next_polarity, next_step))
 
         print(f"Num possibilities: {end_count}")
 
 
-    def _find_start(self) -> Pos2D:
+    def _find_start(self) -> ChunkPos:
         for y in range(0, len(self.grid)):
             row = self.grid[y]
             for x in range(0, len(row)):
                 if row[x] == 'S':
-                    return Pos2D(x, y)
+                    return ChunkPos(0, 0, x, y)
         raise RuntimeError('Starting pos not found')
